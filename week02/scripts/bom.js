f2793ea1b0bb275bd81ea51ca2745d216db65a8b
Benjamin265-sm <you@example.com>
2025-07-08 01:15:50 -0700

    dom

---
+++ week02/scripts/bom.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('favchapter');
+const button = document.querySelector('button');
+const list = document.querySelector('_______');
+
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
