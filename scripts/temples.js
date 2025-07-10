c290fa6348cc31f295b198a6fefafb315909548f
Benjamin265-sm <you@example.com>
2025-07-10 11:47:31 -0700

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,11 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const yearSpan = document.getElementById("currentyear");
-    if (yearSpan) {
-        yearSpan.textContent = new Date().getFullYear();
-    }
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const title = document.querySelector('#title');
+const header = document.querySelector("header");
+const yearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
 
-        const lastModifiedP = document.getElementById("lastModified");
-    if (lastModifiedP) {
-        lastModifiedP.textContent = `Last Modified: ${document.lastModified}`;
-    }
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    header.classList.toggle("open");
+    title.classList.toggle('hide');
+
 });
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modified " + lastModified;
